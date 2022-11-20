cc5fb7be94fb61bfb53ba36534a0d8be00cc5006
jaemin72 <ljm005302@gmail.com>
2022-11-20 22:08:52 +0900

    effect를 사용해야하는 이유를 공부해보자
    const [counter, setValue] = useState(0);
    -> 리액트 앱으로 하는 중이라 앞에 React 안붙여도됨
    
    state를 변경할 때 ‘모든’ code 들을 항상 다시 실행됨
    첫 번째 render에만 코드가 실행되고 다른 state변화에는 실행되지 않도록 만들자
    예) API를 통해 데이터를 가져올 때 컴포넌트 렌더에서 API를 부르고
    이후 상태가 변화할 때 그 API에서 데이터를 다시 가져오지 않게 만들 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState} from "react";
+import { render } from "@testing-library/react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log(render)
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text={"hjihi"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick={onClick} text={"click me"} />
     </div>
   );
 }
