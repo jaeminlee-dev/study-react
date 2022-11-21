271522a8d57fae03a9739db4bfb462016c460183
jaemin72 <ljm005302@gmail.com>
2022-11-21 20:28:23 +0900

    effect 개요
    useEffect
    - 두 개의 argument를 가지는 함수
    - 첫 번째 argument는 우리가 딱 한번만 실행하고 싶은 코드
    - 두 번째는 [] 배열을 넣어줌
    -> useEffect가 컴포넌트의 첫 번째 렌더 시점에 iRunOnlyOnce 함수 호출
    그리고 상태를 변화시키면 iRunOnlyOnce는 호출되지 않음
    즉, 한번만 렌더링 됨
    단순화 하여 useEffect(() => {
    console.log("CALL THE API")
    },[]); 써도 됨
    
    참고로 useEffect는 화면이 다 그려지고 나서 실행됩니다. 즉 화면을 먼저 그리고 그다음 실행되요~
    따로 생명주기는 언급안하셔서 글 남깁니다.
    memo랑 헷갈리시는 분이 계시는데 이건 라이프 사이클이랑 연관이 있는 함수이고, 최초 실행만 할 것이냐, 아님 props, state에 따라(언급한 마법) 렌더링시 다시 그릴것이냐 판단하는 함수인듯 합니다.
    class 문법 라이프 사이클 펑션을 함축해놓은게 useEffect 입니다

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import {useState} from "react";
-import { render } from "@testing-library/react";
+import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log(render)
+  console.log("i run every time");
+  const iRunOnlyOnce = () => console.log("I run only once");
+  useEffect(iRunOnlyOnce, [])
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
